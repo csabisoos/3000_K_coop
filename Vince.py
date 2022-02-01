debcaaa8d878225170b5ffcff0e7732d2f38ff15
Balogh Vince <baloghvince.bv@gmail.com>
2022-02-01 10:39:45 +0100

    44. feladat kész

--- Vince.py
+++ Vince.py
@@ -11,7 +11,7 @@ def feladat_42(lista):
 
 # 43)	A program olvasson be egy csapat nevet és írja ki, a csapat hányszor nyert vb-t!
 
-def feladat_42(lista):
+def feladat_43(lista):
     temp = input()
     temp2 = 0
     for i in lista:
@@ -19,3 +19,13 @@ def feladat_42(lista):
             if i.helyezes == 1:
                 temp2 += 1
     return temp2
+
+# 44)	Melyik csapatok nyertek az Angiában rendezett vb-ken? A csapatok neve mellett az évszámot is írja ki!
+
+def feladat_44(lista):
+    temp = []
+    for i in lista:
+        if i.helyszín == "Anglia":
+            temp.append((i.helyszín, i.ev))
+    return temp
+
