a87c6cf962ccd4983241a209928611a073a336ea
Balogh Vince <baloghvince.bv@gmail.com>
2022-02-01 11:01:02 +0100

    50. feladat kész

--- Vince.py
+++ Vince.py
@@ -1,12 +1,13 @@
+from operator import contains
+
 # 42)	A program olvasson be egy csapat nevet és írja ki, a csapat vb-n elért legjobb helyezését!
 
 def feladat_42(lista):
     temp = input("Adjon meg egy csapat nevet: ")
     temp2 = 100
     for i in lista:
-        if i.orszag == temp:
-            if i.helyezes < temp2:
-                temp2 = i.helyezes
+        if i.orszag == temp and i.helyezes < temp2:
+            temp2 = i.helyezes
     return temp2
 
 # 43)	A program olvasson be egy csapat nevet és írja ki, a csapat hányszor nyert vb-t!
@@ -15,9 +16,8 @@ def feladat_43(lista):
     temp = input("Adjon meg egy csapat nevet: ")
     temp2 = 0
     for i in lista:
-        if i.orszag == temp:
-            if i.helyezes == 1:
-                temp2 += 1
+        if i.orszag == temp and i.helyezes == 1:
+            temp2 += 1
     return temp2
 
 # 44)	Melyik csapatok nyertek az Angiában rendezett vb-ken? A csapatok neve mellett az évszámot is írja ki!
@@ -57,3 +57,17 @@ def feladat_48(lista):
 
 def feladat_49(lista):
     return IttNyertCsapatok(lista, input("Adjon meg egy ország nevet: "))
+
+# 50)	Melyik csapat nyerte a vb-t, amikor Magyarország dobogós helyzést ért el? A győzetes csapatok neve mellett az évszámot is írja ki!
+
+def VBtNyertAmikorXYDobogósLett(lista, ország):
+    temp = []
+    temp2 = []
+    for i in lista:
+        if i.orszag == ország and i.helyezes <=3 and 1 <= i.helyezes and not temp.contains(i.ev):
+            temp.append(i.ev)
+    for i in lista:
+        for j in temp:
+            if i.ev == j and i.helyezes == 1:
+                temp2.append((i.orszag, i.ev))
+    return temp2
