269aeb41d785c096eff1b484556f926625fc06b3
Dániel Fodor <fodor.daniel04@gmail.com>
2022-02-01 10:52:26 +0100

    fodorkeszdenemteljesen

--- fodor.py
+++ fodor.py
@@ -13,11 +13,98 @@ with open("input.txt", "r", encoding="utf8") as f:
         s = sor.strip().split("\t")
         t = Csapat(int(s[0]), s[1], int(s[2]), int(s[3]), s[4])
 
-def fodor1feladat(lista):
-    listasd = []
+def feladat_1_6(lista, ország):
     for elem in lista:
-        if elem.orszag == "Magyarország":
-            listasd.append((f"{elem.helyezes} ,  {elem.ev} ,  {elem.helyszin}"))
-    return listasd
+        if elem.orszag == ország:
+            print(f"{elem.helyezes}  {elem.ev}  {elem.helyszin}")
 
-print(fodor1feladat(Csapat.lista))
+print("----------------------------------------------------------------")
+print("1.Feladat")
+feladat_1_6(Csapat.lista, "Magyarország")
+print("----------------------------------------------------------------")
+print("2.Feladat")
+feladat_1_6(Csapat.lista, "Anglia")
+print("----------------------------------------------------------------")
+print("3.Feladat")
+feladat_1_6(Csapat.lista, "Chile")
+print("----------------------------------------------------------------")
+print("4.Feladat")
+feladat_1_6(Csapat.lista, "Peru")
+print("----------------------------------------------------------------")
+print("5.Feladat")
+feladat_1_6(Csapat.lista, "Mongolia")
+print("----------------------------------------------------------------")
+print("6.Feladat")
+#feladat_1_6(Csapat.lista, "READLINE")
+
+def feladat_7_12(lista, évtized):
+    for elem in lista:
+        if elem.helyezes == 1 and (elem.ev - 1900 >= évtized and elem.ev - 1900 < évtized + 10):
+            print(f"{elem.orszag}  {elem.ev}")
+
+print("----------------------------------------------------------------")
+print("7.Feladat")
+feladat_7_12(Csapat.lista, 30)
+print("----------------------------------------------------------------")
+print("8.Feladat")
+feladat_7_12(Csapat.lista, 40)
+print("----------------------------------------------------------------")
+print("9.Feladat")
+feladat_7_12(Csapat.lista, 50)
+print("----------------------------------------------------------------")
+print("10.Feladat")
+feladat_7_12(Csapat.lista, 60)
+print("----------------------------------------------------------------")
+print("11.Feladat")
+feladat_7_12(Csapat.lista, 70)
+print("----------------------------------------------------------------")
+print("12.Feladat")
+feladat_7_12(Csapat.lista, 80)
+
+def feladat_13_18(lista, ország):
+    sum = 0;
+    for elem in lista:
+        if elem.orszag == ország:
+            #print(f"{elem.orszag}  {elem.helyezes}  {elem.ev}  {elem.helyszin}")
+            sum += 1
+    print(sum)
+
+print("----------------------------------------------------------------")
+print("13.Feladat")
+feladat_13_18(Csapat.lista, "Magyarország")
+print("----------------------------------------------------------------")
+print("14.Feladat")
+feladat_13_18(Csapat.lista, "Anglia")
+print("----------------------------------------------------------------")
+print("15.Feladat")
+feladat_13_18(Csapat.lista, "Chile")
+print("----------------------------------------------------------------")
+print("16.Feladat")
+feladat_13_18(Csapat.lista, "Peru")
+print("----------------------------------------------------------------")
+print("17.Feladat")
+feladat_13_18(Csapat.lista, "Mongólia")
+print("----------------------------------------------------------------")
+print("18.Feladat")
+#feladat_13_18(Csapat.lista, "READLINE")
+
+def feladat_19_23(lista, év):
+    for elem in lista:
+        if elem.ev == év and elem.helyezes == 1:
+            print(f"{elem.orszag}")
+
+print("----------------------------------------------------------------")
+print("19.Feladat")
+feladat_19_23(Csapat.lista, 1930)
+print("----------------------------------------------------------------")
+print("20.Feladat")
+feladat_19_23(Csapat.lista, 1940)
+print("----------------------------------------------------------------")
+print("21.Feladat")
+feladat_19_23(Csapat.lista, 1950)
+print("----------------------------------------------------------------")
+print("22.Feladat")
+feladat_19_23(Csapat.lista, 1960)
+print("----------------------------------------------------------------")
+print("23.Feladat")
+feladat_19_23(Csapat.lista, 1970)
