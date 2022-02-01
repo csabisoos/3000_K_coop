b82d8eb75742e5d83866328c68c620db86cf64b7
Balogh Vince <baloghvince.bv@gmail.com>
2022-02-01 10:36:54 +0100

    43. feladat kész

--- Vince.py
+++ Vince.py
@@ -8,3 +8,14 @@ def feladat_42(lista):
             if i.helyezes < temp2:
                 temp2 = i.helyezes
     return temp2
+
+# 43)	A program olvasson be egy csapat nevet és írja ki, a csapat hányszor nyert vb-t!
+
+def feladat_42(lista):
+    temp = input()
+    temp2 = 0
+    for i in lista:
+        if i.orszag == temp:
+            if i.helyezes == 1:
+                temp2 += 1
+    return temp2
