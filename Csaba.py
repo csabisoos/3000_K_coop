c5592a9ecd29ab439aa4a09ccc719530c2fb7ca3
Csaba Soós <csabi.soos.05@gmail.com>
2022-02-04 12:59:25 +0100

    beolvasas

--- Csaba.py
+++ Csaba.py
@@ -1,3 +1,18 @@
+class Csapat:
+    lista = []
+    def __init__(self, sorszam, orszag, helyezes, ev, helyszin):
+        self.sorszam = sorszam
+        self.orszag = orszag
+        self.helyezes = helyezes
+        self.ev = ev
+        self.helyszin = helyszin
+        Csapat.lista.append(self)
+
+with open("input.txt", "r", encoding="utf8") as f:
+    for sor in f:
+        s = sor.strip().split("\t")
+        t = Csapat(int(s[0]), s[1], int(s[2]), int(s[3]), s[4])
+
 def feladat_24(lista):
     x = 0
     for elem in lista:
